56fd754cd03f87281f7f6c776714e491e329b4ce
kosmo <kosmolebryce@pm.me>
2024-07-02 21:06:59 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple JS for visual effects or smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
